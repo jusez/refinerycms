3988e1d7b411e8d998ef767556f4abd4891a213b
Jussi <jussi@iwa.fi>
2013-07-31 16:44:10 +0300

    Reverted back to original

--- core/app/models/refinery/core/base_model.rb
+++ core/app/models/refinery/core/base_model.rb
@@ -3,7 +3,6 @@ require 'active_record'
 module Refinery
   module Core
     class BaseModel < ActiveRecord::Base
-      establish_connection "#{Rails.env}.sqlite3"
       self.abstract_class = true
 
     end
