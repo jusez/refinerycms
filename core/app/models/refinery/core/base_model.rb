431fcbebac099d1e03abbe9919536091f9c2d234
Jussi <jussi@iwa.fi>
2013-07-31 16:34:41 +0300

    Testing double database connection

--- core/app/models/refinery/core/base_model.rb
+++ core/app/models/refinery/core/base_model.rb
@@ -3,7 +3,7 @@ require 'active_record'
 module Refinery
   module Core
     class BaseModel < ActiveRecord::Base
-
+      establish_connection "#{Rails.env}.sqlite3"
       self.abstract_class = true
 
     end
